a2af6d9a4b5cdc85b675e4a017c293c0f08dbb17
Ramya <Ramya@gmail>
2026-01-19 18:31:09 +0530

    changes are commited

---
+++ firstspringbootproject/src/main/java/com/twg/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.twg.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.twg.springboot.entities.Employee;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ firstspringbootproject/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ taglib uri="jakarta.tags.core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="/style.css" />
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Hello---${msg}</h1>
+</body>
+</html>
